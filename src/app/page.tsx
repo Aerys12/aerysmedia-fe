7c193bdaa9db10caeb75e138b9cc836ad45e7f76
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2024-05-07 16:14:27 -0400

    dashhboard header completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import DashboardAside from "@/components/DashboardAside";
+import DashboardHeader from "@/components/DashboardHeader";
 
 export default function Dashboard() {
 	return (
 		<div className='flex min-h-screen w-full flex-col bg-muted/40'>
 			<DashboardAside />
+      <DashboardHeader />
 		</div>
 	);
 }
--- src/components/DashboardAside.tsx
+++ src/components/DashboardAside.tsx
@@ -5,6 +5,7 @@ import {
 	GalleryVerticalEnd,
 	Images,
 	Package,
+	Settings,
 } from "lucide-react";
 import {
 	Tooltip,
@@ -29,7 +30,7 @@ export default function DashboardAside() {
 						<TooltipTrigger asChild>
 							<Link
 								href='#'
-								className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+								className='flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground  transition-colors hover:text-foreground md:h-8 md:w-8'
 							>
 								<Home className='h-5 w-5' />
 								<span className='sr-only'>Dashboard</span>
@@ -43,7 +44,7 @@ export default function DashboardAside() {
 						<TooltipTrigger asChild>
 							<Link
 								href='#'
-								className='flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+								className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground  transition-colors hover:text-foreground md:h-8 md:w-8'
 							>
 								<Package className='h-5 w-5' />
 								<span className='sr-only'>Services</span>
@@ -78,6 +79,22 @@ export default function DashboardAside() {
 							</Link>
 						</TooltipTrigger>
 						<TooltipContent side='right'>Images</TooltipContent>
+					</Tooltip>
+				</TooltipProvider>
+			</nav>
+			<nav className='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>
+				<TooltipProvider>
+					<Tooltip>
+						<TooltipTrigger asChild>
+							<Link
+								href='#'
+								className='flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8'
+							>
+								<Settings className='h-5 w-5' />
+								<span className='sr-only'>Settings</span>
+							</Link>
+						</TooltipTrigger>
+						<TooltipContent side='right'>Settings</TooltipContent>
 					</Tooltip>
 				</TooltipProvider>
 			</nav>

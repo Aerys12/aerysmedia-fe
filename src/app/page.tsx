4087b86ef6a6eee8ebf01a426ea9abfde15da0ba
Aerys12 <aerys.nsubuga@mail.utoronto.ca>
2024-05-07 13:24:07 -0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div className='flex min-h-screen w-full flex-col bg-muted/40'>
+			<h1>Dashboard in progress</h1>
+		</div>
+	);
+}
